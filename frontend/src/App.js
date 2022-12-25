0d4765006ed9d63b5e79cb606ad369c22eb4dc24
arsenky <arsenky@yandex.ru>
2022-12-25 15:18:03 +0300

    lesson_2_complited

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,32 +5,32 @@ import AuthorList from './components/Author.js'
 import axios from 'axios'
 
 class App extends React.Component {
-constructor(props) {
-super(props)
-this.state = {
-'authors': []
-}
-}
-componentDidMount() {
-  axios.get('http://127.0.0.1:8000/api/authors')
-  .then(response => {
-  const authors = response.data
-  this.setState(
-  {
-  'authors': authors
+  constructor(props) {
+    super(props)
+    this.state = {
+      'authors': []
+    }
   }
-  )
-  }).catch(error => console.log(error))
+
+  componentDidMount() {
+    axios.get('http://127.0.0.1:8000/api/authors')
+      .then(response => {
+        const authors = response.data
+          this.setState(
+            {
+            'authors': authors
+            }
+          )
+      }).catch(error => console.log(error))
   }
-  
+
   render () {
     return (
-    <div>
-    <AuthorList authors={this.state.authors} />
-    </div>
+      <div>
+        <AuthorList authors={this.state.authors} />
+      </div>
     )
-    }
+  }
 }
-
 
 export default App;
